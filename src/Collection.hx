f3cac71b7bf3256ba81cfacb222b438241993f2f
Koji MUKOYAMA <bp12116@shibaura-it.ac.jp>
2014-11-06 17:06:33 +0900

    [WIP]distinctとzipを実装したが、flattenができない

--- src/Collection.hx
+++ src/Collection.hx
@@ -2,29 +2,37 @@ package ;
 
 class Collection
 {
-	public function new(){
+	public static function distinct<T>(it : Array<Dynamic>):Array<Dynamic>{
+		var distincted = new Array<Dynamic>();
+		for(x in it){
+			var judge = true;
+			for(a in distincted){
+				if(x == a) judge = false;
+			}
+			if(judge)	distincted.push(x);
+		}
+		return distincted;
 	}
-
-	public static function distinct<T>(it : Iterable<T>):List<T>{
-		var l = new List();
+	
+	public static function flatten<A>(it : Array<Dynamic>):Array<Dynamic>{
+		var l = new Array<Dynamic>();
 		
+		for(a in it){
+			//if(Std.is(a,Array)){
+				//for(item in a){
+					//l.push(item);
+			//	}
+			//}
+			/*else*/ l.push(a);
+		}
 		return l;
 	}
 	
-	public static function flatten<T>(it : Iterable<T>):List<T>{
-		var l = new List();
-		
-		return l;
-	}
-	
-	public static function zip<A,B>(a : Array<A>, b : Array<B>):Array<Dynamic>{
+	public static function zip<A,B>(a : Array<Dynamic>, b : Array<Dynamic>):Array<Dynamic>{
 		var zipped = new Array<Dynamic>();
 		var array:Array<Dynamic> = [];
-		for(i in a){
-		
-			for(j in b){
-				array = [i,j];
-			}
+		for(i in 0...a.length){
+			array = [a[i],b[i]];
 			zipped.push(array);
 		}
 		return zipped;
--- src/Main.hx
+++ src/Main.hx
@@ -4,11 +4,19 @@ using Collection;
 class Main
 {
 public static function main(){
-	//var Collection = new Collection();
+	
 	var array1 = [10,20,30,40,50];
 	var array2 = ["abc","def","ghi","jkl","mno"];
 	var result = Collection.zip(array1,array2);
 	trace(result);
 	trace(array2);
+	
+	var flatten = [[1,2],[3,4]];
+	var flaResult = Collection.flatten(flatten);
+	trace(flaResult);
+	
+	var distinct = [10,20,30,40,50,10,60];
+	var disResult = Collection.distinct(distinct);
+	trace(disResult);
 }
 }
