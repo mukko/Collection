331466440b4a8f5e9d12fc87f4f8d1d47a787ed6
Koji MUKOYAMA <bp12116@shibaura-it.ac.jp>
2014-11-07 10:24:44 +0900

    Collectionのすべての動作を正しく動くようになった
    あとは綺麗なコードにできればする方向

--- src/Main.hx
+++ src/Main.hx
@@ -6,17 +6,18 @@ class Main
 public static function main(){
 	
 	var array1 = [10,20,30,40,50];
-	var array2 = ["abc","def","ghi","jkl","mno"];
+	var array2 = ["abc","def","ghi","jkl"];
 	var result = Collection.zip(array1,array2);
 	trace(result);
 	trace(array2);
 	
-	var flatten = [[1,2],[3,4]];
+	var flatten:Array<Dynamic> = [[[1,2],[3,4]],5];
 	var flaResult = Collection.flatten(flatten);
 	trace(flaResult);
 	
 	var distinct = [10,20,30,40,50,10,60];
 	var disResult = Collection.distinct(distinct);
 	trace(disResult);
+
 }
 }
