18ed75f009744d66ac3728b62d2bb76021d79825
mukko <kyo22943@gol.com>
2014-11-07 10:24:44 +0900

    Collectionのすべての動作を正しく動くようになった
    あとは綺麗なコードにできればする方向

--- src/Collection.hx
+++ src/Collection.hx
@@ -2,37 +2,47 @@ package ;
 
 class Collection
 {
-	public static function distinct<T>(it : Array<Dynamic>):Array<Dynamic>{
+	public static function distinct<Dynamic>(it:Array<Dynamic>):Array<Dynamic>
+	{
 		var distincted = new Array<Dynamic>();
-		for(x in it){
+		for (x in it)
+		{
 			var judge = true;
-			for(a in distincted){
-				if(x == a) judge = false;
+			for (a in distincted)
+			{
+				if (x == a) judge = false;
 			}
-			if(judge)	distincted.push(x);
+			if (judge) distincted.push(x);
 		}
 		return distincted;
 	}
-	
-	public static function flatten<A>(it : Array<Dynamic>):Array<Dynamic>{
-		var l = new Array<Dynamic>();
-		
-		for(a in it){
-			//if(Std.is(a,Array)){
-				//for(item in a){
-					//l.push(item);
-			//	}
-			//}
-			/*else*/ l.push(a);
+
+	public static function flatten<Dynamic>(it:Array<Dynamic>):Array<Dynamic>
+	{
+		var flattened = new Array<Dynamic>();
+
+		for (a in it)
+		{
+			if (Std.is(a, Array))
+			{
+				var innerArray:Array<Dynamic> = cast(a);
+				for (i in 0...innerArray.length)
+				{
+					flattened.push(innerArray[i]);
+				}
+			} else flattened.push(a);
 		}
-		return l;
+		return flattened;
 	}
-	
-	public static function zip<A,B>(a : Array<Dynamic>, b : Array<Dynamic>):Array<Dynamic>{
+
+	public static function zip<A, B>(a:Array<Dynamic>, b:Array<Dynamic>):Array<Dynamic>
+	{
 		var zipped = new Array<Dynamic>();
 		var array:Array<Dynamic> = [];
-		for(i in 0...a.length){
-			array = [a[i],b[i]];
+		for (i in 0...a.length)
+		{
+			if (b.length <= i) break;
+			array = [a[i], b[i]];
 			zipped.push(array);
 		}
 		return zipped;
